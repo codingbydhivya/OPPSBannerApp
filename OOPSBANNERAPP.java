5f81ac4a661093809d6ed9f584a9f0c03682e002
codingbydhivya <dh3127@srmist.edu.in>
2026-03-11 10:01:02 +0530

    oops banner unsing string.join

--- OOPSBANNERAPP.java
+++ OOPSBANNERAPP.java
@@ -1,11 +1,11 @@
 public class OPPSBANNERAPP{
 public static void main(String[],args0{
- System.out.println("   ***   " + "     " + "   ***   " + "     " + "*******  " + "     " + "   ***** ");
- System.out.println(" **   ** " + "     " + " **   ** " + "     " + "**    ** " + "     " + " **    ");
- System.out.println("**     **" + "     " + "**     **" + "     " + "**    ***" + "     " + "**       ");
- System.out.println("**     **" + "     " + "**     **" + "     " + "**    ** " + "     " + "  *****  ");
- System.out.println("**     **" + "     " + "**     **" + "     " + "*******  " + "     " + "      ***");
- System.out.println(" **   ** " + "     " + " **   ** " + "     " + "**       " + "     " + "     *** ");
- System.out.println("   ***  " + "     " + "    ***   " + "     " + "**       " + "     " + " ******  ");
+System.out.println(String.join("     ", "   ***   ", "   ***   ", "*******  ", "   ***** "));
+System.out.println(String.join("     ", " **   ** ", " **   ** ", "**    ** ", " **    "));
+System.out.println(String.join("     ", "**     **", "**     **", "**    ***", "**       "));
+System.out.println(String.join("     ", "**     **", "**     **", "**    ** ", "  *****  "));
+System.out.println(String.join("     ", "**     **", "**     **", "*******  ", "      ***"));
+System.out.println(String.join("     ", " **   ** ", " **   ** ", "**       ", "     *** "));
+System.out.println(String.join("     ", "   ***  ", "    ***   ", "**       ", " ******  "));
 }}
    
