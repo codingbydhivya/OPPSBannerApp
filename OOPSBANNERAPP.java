cecb2f594018d5300750806554c8ab0d730adb75
codingbydhivya <dh3127@srmist.edu.in>
2026-03-11 09:48:00 +0530

    OOPS banner unsing multiple print statements

---
+++ OOPSBANNERAPP.java
@@ -0,0 +1,11 @@
+public class OPPSBANNERAPP{
+public static void main(String[],args0{
+ System.out.println("   ***   " + "     " + "   ***   " + "     " + "*******  " + "     " + "   ***** ");
+ System.out.println(" **   ** " + "     " + " **   ** " + "     " + "**    ** " + "     " + " **    ");
+ System.out.println("**     **" + "     " + "**     **" + "     " + "**    ***" + "     " + "**       ");
+ System.out.println("**     **" + "     " + "**     **" + "     " + "**    ** " + "     " + "  *****  ");
+ System.out.println("**     **" + "     " + "**     **" + "     " + "*******  " + "     " + "      ***");
+ System.out.println(" **   ** " + "     " + " **   ** " + "     " + "**       " + "     " + "     *** ");
+ System.out.println("   ***  " + "     " + "    ***   " + "     " + "**       " + "     " + " ******  ");
+}}
+   
